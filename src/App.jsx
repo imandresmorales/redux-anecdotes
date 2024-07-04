e2902525119c45aea3c1f923f760f1e849803fed
Andres Morales <moralesandres@outlook.com>
2024-07-04 15:56:26 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import { useSelector, useDispatch } from 'react-redux'
+
+const App = () => {
+  const anecdotes = useSelector(state => state)
+  const dispatch = useDispatch()
+
+  const createAnecdote = (id) => {
+    return {
+      type: 'VOTE',
+      payload: {
+        id: id
+      }    
+    }
+  } 
+
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      {anecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => dispatch(createAnecdote(anecdote.id))}>vote</button>
+          </div>
+        </div>
+      )}
+      <h2>create new</h2>
+      <form>
+        <div><input /></div>
+        <button>create</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
